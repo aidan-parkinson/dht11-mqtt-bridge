8dc20094be424d979cbdc7e0b3e32e81f9c4d753
aidan-parkinson <aidan.parkinson@gmail.com>
2021-01-30 17:10:09 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # dht11-mqtt-bridge
 An executable python function for publishing DHT11 temperature and humidity data from a Raspberry Pi using Paho MQTT
+
+Download the latest ca certificate to appropriate folder
+cd /etc/ssl/certs
+sudo rm learning-iot-ca.crt
+sudo curl -0 https://learning-iot.aidanparkinson.xyz -o learning-iot-ca.crt
+
+## Install device dependencies
+```
+sudo apt update
+sudo apt-get install virtualenv python3-pip
+pip3 --version
+```
+
+## Clone this repository
+Navigate to add-on application folder
+```
+cd /opt
+```
+Clone repository
+```
+sudo git clone https://github.com/aidan-parkinson/dht11-mqtt-bridge.git
+```
+
+## Create virtual environment and install project dependencies
+Navigate to project folder
+```
+cd /opt/dht11-mqtt-bridge/
+```
+Create a virtual environment
+```
+sudo virtualenv venv
+```
+Activate it
+```
+source venv/bin/activate
+```
+Install project dependencies
+```
+pip3 install -r requirements.txt
+```
+Deactivate the virtual environment
+```
+deactivate
+```
+
+## To run the bridge (start at this point if you have completed this process before on your device)
+Navigate to project folder
+```
+cd /opt/dht11-mqtt-bridge/
+```
+Activate the virtual evironment with pre-installed dependencies
+```
+source venv/bin/activate
+```
+Run the `dht11-mqtt.py` controller
+```
+sudo python3 dht11-mqtt.py
+```
---
+++ dht11-mqtt.py
@@ -0,0 +1,54 @@
+import paho.mqtt.client as mqtt
+import ssl
+from getmac import get_mac_address as gma
+
+import adafruit_dht
+import time
+import board
+import json
+from datetime import datetime
+
+pin = board.D4
+
+dht_device = adafruit_dht.DHT11(pin)
+
+client = mqtt.Client()
+
+broker = 'mqtts.aidanparkinson.xyz'
+port = 8883
+client.tls_set('/etc/ssl/certs/learning-iot-ca.crt', cert_reqs=ssl.CERT_NONE)
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client.connect(broker, port)
+client.loop_start();
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+while True:
+
+    try:
+        humidity = dht_device.humidity
+        temperature = dht_device.temperature
+
+        reading = json.dumps({'mac_address': gma, 'humidity': humidity, 'temperature': temperature, 'datetime': datetime.now()}, sort_keys=True, indent=4, default=str)
+
+        print(reading)
+
+        client.publish("dht11", reading)
+
+    except RuntimeError:
+        print('Failed')
+
+    time.sleep(5);
